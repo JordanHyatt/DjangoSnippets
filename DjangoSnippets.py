80fc9d573969faaf37894a3f40b43f5c90f54004
Jordan Hyatt <38536669+JordanHyatt@users.noreply.github.com>
2020-01-05 10:51:27 -0500

    Feature added
    Mixin now inherets from SingleTableMixin in which allows the mixin to also save the users sort selection in addition to their filter selection.

--- DjangoSnippets.py
+++ DjangoSnippets.py
@@ -1,4 +1,4 @@
-class SaveFilterMixin:
+class SaveFilterMixin(SingleTableMixin):
     """ This Mixin Can be used with a FilterView SingleTable in order to save
     the users filter selections after navegating away from the lookup page"""
     def get_filterset_kwargs(self,*args):
@@ -6,13 +6,18 @@ class SaveFilterMixin:
         kwargs = super().get_filterset_kwargs(*args)
         if kwargs['data']:
             filter_data = kwargs['data']
+            table_sort = filter_data.get('sort')
             self.request.session[f'{class_name}_filter_data']=filter_data
-            print(self.request.session[f'{class_name}_filter_data'])
+            self.request.session[f'{class_name}_table_sort']=table_sort
         else:
             if f'{class_name}_filter_data' in self.request.session.keys():
                 kwargs['data']=self.request.session[f'{class_name}_filter_data']
         return kwargs  
 
     def get_table_data(self,*args,**kwargs):
-        print(self.get_filterset(self.filterset_class).qs)
         return self.get_filterset(self.filterset_class).qs
+
+    def get_table_kwargs(self,**kwargs):
+        class_name = self.__class__
+        kwargs['order_by']=self.request.session.get(f'{class_name}_table_sort')
+        return kwargs
