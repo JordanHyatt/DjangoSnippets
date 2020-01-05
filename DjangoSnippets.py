076087cecc850d93066e2f8a763f52c86e92c43f
Jordan Hyatt <38536669+JordanHyatt@users.noreply.github.com>
2020-01-05 06:06:07 -0500

    Bug Fix
    Updated code so that the key used in the user sessions dictionary tags includes the Parent's class name.  This resolves overwriting conflicts when using the mixin in multiple views

--- DjangoSnippets.py
+++ DjangoSnippets.py
@@ -1,16 +1,18 @@
 class SaveFilterMixin:
-    """ This Mixin Can be used with a FilterView in order to save
+    """ This Mixin Can be used with a FilterView SingleTable in order to save
     the users filter selections after navegating away from the lookup page"""
-    
     def get_filterset_kwargs(self,*args):
+        class_name = self.__class__
         kwargs = super().get_filterset_kwargs(*args)
         if kwargs['data']:
-            bucket_filter_data = kwargs['data']
-            self.request.session['table_filter_data']= bucket_filter_data
+            filter_data = kwargs['data']
+            self.request.session[f'{class_name}_filter_data']=filter_data
+            print(self.request.session[f'{class_name}_filter_data'])
         else:
-            if 'table_filter_data' in self.request.session.keys():
-                kwargs['data']=self.request.session['table_filter_data']
+            if f'{class_name}_filter_data' in self.request.session.keys():
+                kwargs['data']=self.request.session[f'{class_name}_filter_data']
         return kwargs  
 
     def get_table_data(self,*args,**kwargs):
+        print(self.get_filterset(self.filterset_class).qs)
         return self.get_filterset(self.filterset_class).qs
