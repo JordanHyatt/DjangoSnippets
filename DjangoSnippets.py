126a80f964a6dcd921d106937ae25873c4ef52e0
Jordan Hyatt <38536669+JordanHyatt@users.noreply.github.com>
2020-03-26 10:20:20 -0400

    Update DjangoSnippets.py

--- DjangoSnippets.py
+++ DjangoSnippets.py
@@ -21,3 +21,25 @@ class SaveFilterMixin(SingleTableMixin):
         class_name = self.__class__
         kwargs['order_by']=self.request.session.get(f'{class_name}_table_sort')
         return kwargs
+    
+class SaveFormMixin:
+    """ This Mixin Can be used with any view that uses a form mixin to 
+    save a users form selection even if they navigate away from the page"""
+    
+    def __init__(self,*args,**kwargs):
+        super().__init__(*args,**kwargs)
+        self.view_name = self.__class__
+
+    def get_form(self,**kwargs):
+        form = super().get_form(**kwargs)
+        if self.request.method == 'GET':
+            if len(self.request.GET) > 0:
+                qd=self.request.GET
+                self.request.session[f'{self.view_name}_form_initial']=qd
+            initial=self.request.session.get(f'{self.view_name}_form_initial')
+            if initial not in [{},None]:
+                form = self.form_class(initial=initial)
+            
+        if self.request.method == 'POST':
+            self.request.session[f'{self.view_name}_form_initial']=form.data
+        return form
